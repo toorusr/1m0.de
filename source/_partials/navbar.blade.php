4fcef5ebea899c5cc532ca858653f3f44703c66b
Toorusr <toorusr@protonmail.com>
2018-05-20 18:18:32 +0200

    Add: navbar & footer blades

---
+++ source/_partials/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class="md:flex items-center justify-between flex-wrap bg-white p-6">
+    <div class="flex items-center justify-center md:justify-start flex-no-shrink text-white mr-6">
+        <a href="/">
+            <img src="/img/icon/logo.png" class="w-16" alt="1m0 logo" />
+        </a>
+    </div>
+    <div class="w-full block items-center justify-center flex-grow md:flex  md:items-center md:w-auto">
+        <div>
+
+        </div>
+    </div>
+</nav>
