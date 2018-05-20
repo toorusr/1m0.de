6de670e2bff1154c064f87b0c57d08bae6c7cc87
Toorusr <toorusr@protonmail.com>
2018-05-20 18:17:17 +0200

    Add: navbar & footer

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,9 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/css/styles.css">
     </head>
     <body>
-        @yield('body')
+        @include('_partials.navbar')
+        <main>
+            @yield('body')
+        </main>
+        @include('_partials.footer')
     </body>
 </html>
