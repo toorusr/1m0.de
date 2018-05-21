9ee3d30976e368dcffc5ba7265f38f55e778907b
Toorusr <toorusr@protonmail.com>
2018-05-21 14:08:07 +0200

    Add: footer

---
+++ source/_partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="md:flex items-center justify-between flex-wrap bg-white p-6">
+    <div class="w-full">
+        <div class="mt-4 flex" id=social>
+            <img />
+        </div>
+        <div class="bg-grey-lightest text-grey-dark p-8 leading-normal text-center">
+          <p>&copy; 2018 1m0.de
+        </div>
+    </div>
+</footer>
