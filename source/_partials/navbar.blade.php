802daddc866478ddbbb99471609624ad56a9716d
Toorusr <toorusr@protonmail.com>
2018-05-21 14:08:19 +0200

    Add: menu links

--- source/_partials/navbar.blade.php
+++ source/_partials/navbar.blade.php
@@ -5,8 +5,10 @@
         </a>
     </div>
     <div class="w-full block items-center justify-center flex-grow md:flex  md:items-center md:w-auto">
-        <div>
-
+        <div class="text-2xl md:flex-grow text-center md:text-right">
+            <a class="no-underline text-black inline md:block mt-4 md:inline-block md:mt-0 border-b-2 border-blue-light mr-4 p-7">About me</a>
+            <a class="no-underline text-black inline md:block mt-4 md:inline-block md:mt-0 border-b-2 border-red-light mr-4 p-7">Projects</a>
+            <a class="no-underline text-black inline md:block mt-4 md:inline-block md:mt-0 border-b-2 border-green-light mr-4 p-7">Github</a>
         </div>
     </div>
 </nav>
