2a9f918a5ed1c8aa2b3e90cff55e9b05bf3d0a72
Toorusr <toorusr@protonmail.com>
2018-05-22 16:11:41 +0200

    Add: contact link

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -4,7 +4,7 @@
             <img />
         </div>
         <div class="bg-grey-lightest text-grey-dark p-8 leading-normal text-center">
-            <p>&copy; 2018 1m0.de</p>
+            <p>&copy; 2018 1m0.de <a href="/contact" class="no-underline text-grey-dark hover:text-black">[contact]</a></p>
             <p> The source of this site is hosted on <a href="https://github.com/toorusr/1m0.de"><img src="/img/icon/github.svg" class="w-4 sticky pin-b" alt="Github icon" /></a>.</p>
         </div>
     </div>
