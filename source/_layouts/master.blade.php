ff6390edbe852c14d149ebc3f9a1540a9e09b3ba
Toorusr <toorusr@protonmail.com>
2018-05-22 15:11:44 +0200

    Fix: empty 404 title

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="/css/styles.css">
-        <title> 1m0 | @yield('title')</title>
+        @if ($page->getPath() != '/404')
+            <title> 1m0 | @yield('title')</title>
+        @else
+            <title> 1m0 | 404 </title>
+        @endif
     </head>
     <body>
         @include('_partials.navbar')
