28eed2ca1d8af238dfdca06e4b66c4a9bb458e7e
Toorusr <toorusr@protonmail.com>
2018-05-22 15:13:09 +0200

    Add: blog page

---
+++ source/blog.blade.php
@@ -0,0 +1,15 @@
+@extends('_layouts.master')
+
+@section('body')
+    <section class="parallax min-h-screen"></section>
+    <div id=content class="border-t-8 border-green-light min-h-screen">
+        <br>
+        <h1 class="text-center p20">Attention</h1>
+        <p class="text-center text-grey">I am currently editing this site, please try again later.</p>
+        <br>
+    </div>
+@endsection
+
+@section('title')
+    Blog
+@endsection
