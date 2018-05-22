cee6a1a66c69fd829587e4c128f6c846d3652685
Toorusr <toorusr@protonmail.com>
2018-05-22 10:52:13 +0200

    Add: links to buttons

--- source/_partials/navbar.blade.php
+++ source/_partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="md:flex items-center justify-between flex-wrap bg-white p-6">
+<nav class="md:flex items-center justify-between flex-wrap bg-white h-20 p-6 ">
     <div class="flex items-center justify-center md:justify-start flex-no-shrink text-white mr-6">
         <a href="/">
             <img src="/img/icon/logo.png" class="w-16" alt="1m0 logo" />
