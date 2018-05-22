962ff2c4b7030ba1daef0e9e859cfdae23c71570
Toorusr <toorusr@protonmail.com>
2018-05-22 10:51:30 +0200

    Add: links to buttons

--- source/_partials/navbar.blade.php
+++ source/_partials/navbar.blade.php
@@ -6,9 +6,9 @@
     </div>
     <div class="w-full block items-center justify-center flex-grow md:flex  md:items-center md:w-auto">
         <div class="text-2xl md:flex-grow text-center md:text-right">
-            <a class="no-underline text-black inline md:block mt-4 md:inline-block md:mt-0 border-b-2 border-blue-light mr-4 p-7">About me</a>
+            <a href="/aboutme" class="no-underline text-black inline md:block mt-4 md:inline-block md:mt-0 border-b-2 border-blue-light mr-4 p-7">About me</a>
             <a class="no-underline text-black inline md:block mt-4 md:inline-block md:mt-0 border-b-2 border-red-light mr-4 p-7">Projects</a>
-            <a class="no-underline text-black inline md:block mt-4 md:inline-block md:mt-0 border-b-2 border-green-light mr-4 p-7">Github</a>
+            <a class="no-underline text-black inline md:block mt-4 md:inline-block md:mt-0 border-b-2 border-green-light mr-4 p-7">Contact</a>
         </div>
     </div>
 </nav>
