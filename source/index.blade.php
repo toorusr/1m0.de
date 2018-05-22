a412cbf9d9a35bf2acb7846aec737d46b96471d5
Toorusr <toorusr@protonmail.com>
2018-05-22 10:53:28 +0200

    Add: cards & parallax | Cut: underconstruction label

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,8 +1,34 @@
 @extends('_layouts.master')
 
 @section('body')
-<div class="text-center">
-    <h1>1m0</h1>
-    <h6 class="text-grey-darker">UNDER CONSTRUCTION</h6>
-</div>
+    <section class="parallax sm:h-40 min-h-screen"></section>
+    <div class="flex justify-center border-t border-black-light">
+        <div>
+            @component('_components/prevcard')
+                @slot('img') https://unsplash.it/804/800 @endslot
+                @slot('header') About me @endslot
+                @slot('text') Do you want to know more about the person behind the alias 1m0 and toorusr? On this page you can find some facts and stuff about me. @endslot
+                @slot('button') Who are you? @endslot
+                @slot('buttonclr') blue @endslot
+            @endcomponent
+            @component('_components/prevcard')
+                @slot('img') https://unsplash.it/804/800 @endslot
+                @slot('header') Projects @endslot
+                @slot('text') Here you can find all of my projects I am currently working on and thoses I have worked on in the past. @endslot
+                @slot('button') Show me more @endslot
+                @slot('buttonclr') red @endslot
+            @endcomponent
+            @component('_components/prevcard')
+                @slot('img') https://unsplash.it/804/800 @endslot
+                @slot('header') Blog @endslot
+                @slot('text') Just an senseless text. @endslot
+                @slot('button') Lol @endslot
+                @slot('buttonclr') green @endslot
+            @endcomponent
+        </div>
+    </div>
+@endsection
+
+@section('title')
+    Home
 @endsection
