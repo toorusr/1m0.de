5d71c97a63a7eb078f79d0187287bec9a84fcc14
Toorusr <toorusr@protonmail.com>
2018-05-22 10:49:55 +0200

    Add: opensource notice & sticky to bottom

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,10 +1,11 @@
-<footer class="md:flex items-center justify-between flex-wrap bg-white p-6">
-    <div class="w-full">
+<footer class="md:flex items-center justify-between flex-wrap bg-white p-0">
+    <div class="w-full sticky pin-b">
         <div class="mt-4 flex" id=social>
             <img />
         </div>
         <div class="bg-grey-lightest text-grey-dark p-8 leading-normal text-center">
-          <p>&copy; 2018 1m0.de
+            <p>&copy; 2018 1m0.de</p>
+            <p> The source of this site is hosted on <a href="https://github.com/toorusr/1m0.de"><img src="/img/icon/github.svg" class="w-4 sticky pin-b" alt="Github icon" /></a>.</p>
         </div>
     </div>
 </footer>
