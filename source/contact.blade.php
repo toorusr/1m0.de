d44efc74b22048c839a3597e9055e0641f1f837b
Toorusr <toorusr@protonmail.com>
2018-05-31 00:20:18 +0200

    Fix: netlify forms

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     @include('_partials.parallax')
     <div class="flex items-center">
-    <form class="w-full max-w-xs" name="contact" netlify>
+    <form class="w-full max-w-xs" method="POST" name="contact" netlify>
         <div class="md:flex md:items-center mb-6">
             <div class="md:w-1/3">
                 <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
@@ -21,7 +21,7 @@
                 </label>
             </div>
             <div class="md:w-2/3">
-                <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker" type="text" placeholder="example@1m0.de" name="email">
+                <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker" type="email" placeholder="example@1m0.de" name="email">
             </div>
         </div>
         <div class="md:flex md:items-center mb-6">
@@ -31,7 +31,7 @@
                 </label>
             </div>
             <div class="md:w-2/3">
-                <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker" type="text" placeholder="Message..." name="data">
+                <textarea class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker" placeholder="Message..." name="data"></textarea>
             </div>
         </div>
         <div class="md:flex md:items-center mb-6">
@@ -46,7 +46,7 @@
         <div class="md:flex md:items-center">
             <div class="md:w-1/3"></div>
             <div class="md:w-2/3">
-              <button class="shadow bg-purple hover:bg-purple-light text-white font-bold py-2 px-4 rounded" type="button">
+              <button class="shadow bg-purple hover:bg-purple-light text-white font-bold py-2 px-4 rounded" type="submit">
                 Send
               </button>
             </div>
