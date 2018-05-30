8e98b51d770327e0d827f8838f5a6d76f098c0ce
Toorusr <toorusr@protonmail.com>
2018-05-31 00:08:11 +0200

    Fix: contact form && Add: styles & netlify forms

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -2,17 +2,56 @@
 
 @section('body')
     @include('_partials.parallax')
-    <form name="contact" netlify>
-        <p>
-            <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-            <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
-    </form>
+    <div class="flex items-center">
+    <form class="w-full max-w-xs" netlify>
+        <div class="md:flex md:items-center mb-6">
+            <div class="md:w-1/3">
+                <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                    Full Name
+                </label>
+            </div>
+            <div class="md:w-2/3">
+                <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker" type="text" placeholder="Max Mustermann" name="fullName">
+            </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+            <div class="md:w-1/3">
+                <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-username">
+                    Email
+                </label>
+            </div>
+            <div class="md:w-2/3">
+                <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker" type="text" placeholder="example@1m0.de" name="email">
+            </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+            <div class="md:w-1/3">
+                <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                    Data
+                </label>
+            </div>
+            <div class="md:w-2/3">
+                <input class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker" type="text" placeholder="Message..." name="data">
+            </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3"></div>
+            <label class="md:w-2/3 block text-grey font-bold">
+                <input class="mr-2" type="checkbox" name="bug">
+                <span class="text-sm">
+                    I've found a bug
+                </span>
+            </label>
+        </div>
+        <div class="md:flex md:items-center">
+            <div class="md:w-1/3"></div>
+            <div class="md:w-2/3">
+              <button class="shadow bg-purple hover:bg-purple-light text-white font-bold py-2 px-4 rounded" type="button">
+                Send
+              </button>
+            </div>
+        </div>
+</form>
 @endsection
 
 @section('title')
