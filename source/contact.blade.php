e6081cde36ba32a5383dc0f303ea8f79e1d7b506
Toorusr <toorusr@protonmail.com>
2018-05-30 23:41:50 +0200

    Add: contact form

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -2,6 +2,17 @@
 
 @section('body')
     @include('_partials.parallax')
+    <form name="contact" netlify>
+        <p>
+            <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+            <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
 @endsection
 
 @section('title')
