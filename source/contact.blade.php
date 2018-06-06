4ddf9e95260ba911efece758b14d819890be1261
Toorusr <toorusr@protonmail.com>
2018-06-06 18:31:51 +0200

    Add: contact success page

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     @include('_partials.parallax')
     <div class="flex items-center">
-    <form class="w-full max-w-xs" method="POST" name="contact" netlify>
+    <form class="w-full max-w-xs" method="POST" name="contact" action="/contact/success.html" netlify>
         <div class="md:flex md:items-center mb-6">
             <div class="md:w-1/3">
                 <label class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
---
+++ source/success.md
@@ -0,0 +1,9 @@
+---
+extends: _layouts.master
+section: body
+permalink: contact/success.html
+---
+
+<div align="center">
+    <h3> You successfully submitted your message. </h3>
+</div>
