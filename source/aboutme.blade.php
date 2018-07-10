631e7b8fb2509c507c5a7a9342f7744560d55ab8
Toorusr <toorusr@protonmail.com>
2018-07-10 16:21:48 +0200

    Fix: aboutme collection display

--- source/aboutme.blade.php
+++ source/aboutme.blade.php
@@ -7,10 +7,10 @@
         <h1 class="text-center p20">Attention</h1>
         <p class="text-center text-grey">I am currently editing this article, please try again later.</p>
         <br>
-        @foreach ($aboutme as aboutm)
-        	<p class="text-center text-grey">{{$aboutm->last_change}}</p>
-        	{{$aboutm->getContent()}}
-        @endforeach
+    	@foreach($aboutme as $about)
+    	<p class="text-center text-grey">Last Change: {{ $about->last_change }}</p>
+    	<section>{{ $about->getContent() }}</section>
+    	@endforeach
         <br>
     </div>
 @endsection
