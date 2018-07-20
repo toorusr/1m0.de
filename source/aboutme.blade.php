9f1e9259a8618b91cf3f738174055446603adb66
Toorusr <toorusr@protonmail.com>
2018-07-20 04:22:27 +0200

    Fix: use arcticle card

--- source/aboutme.blade.php
+++ source/aboutme.blade.php
@@ -3,15 +3,13 @@
 @section('body')
     @include('_partials.parallax')
     <div id=content class="border-t-8 border-blue-light">
-        <br>
-        <h1 class="text-center p20">Attention</h1>
-        <p class="text-center text-grey">I am currently editing this article, please try again later.</p>
-        <br>
     	@foreach($aboutme as $about)
-    	<p class="text-center text-grey">Last Change: {{ $about->last_change }}</p>
-    	<section>{!! $about->getContent() !!}</section>
-    	@endforeach
-        <br>
+        @component('_components/articlecard')
+            @slot('title') About me @endslot
+            @slot('body') {{ $about->getContent() }} @endslot
+            @slot('last') {{ $about->last_change }} @endslot
+        @endcomponent
+        @endforeach
     </div>
 @endsection
 
